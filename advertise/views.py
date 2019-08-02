3b4bff09e68f5ffbfacc4560752ccec51849361f
ChaeMoon <jungchaemoon@gmail.com>
2019-08-02 16:19:10 +0900

    update signup error message

--- advertise/views.py
+++ advertise/views.py
@@ -1,5 +1,7 @@
 from django.shortcuts import render
 from django.db.models import Q
+from django.db.models.query import QuerySet
+from django.core.exceptions import ImproperlyConfigured
 from django.views.generic import TemplateView, ListView, DetailView
 from django.views.generic.edit import CreateView, DeleteView, UpdateView, FormView
 from .forms import UploadFileForm, PostSearchForm
@@ -35,18 +37,26 @@ class AdvertiseSearchFormView(FormView):
 class AdvertiseRegionIndexView(ListView):
     template_name = 'advertise/advertise_home.html'
     model = Post
-    
-    def get_queryset(self):
-        query = self.kwargs['region_id']
-        if query == 0:
-            object_list = Post.objects.filter(region__icontains='서울')
-            return object_list
-        elif query == 1:
-            object_list = Post.objects.filter(region__icontains='경기도')
-            return object_list
-        elif query ==2:
-            object_list = Post.objects.filter(region__icontains='충청도')
-            return object_list
+
+    # def get_queryset(self):
+    #     filter_word =
+    #     if self.model is not None:
+    #         queryset = self.model.objects.filter(region__area__icontains=filter_word)
+    #     else:
+    #         raise ImproperlyConfigured(
+    #             "%(cls)s is missing a QuerySet. Define "
+    #             "%(cls)s.model, %(cls)s.queryset, or override "
+    #             "%(cls)s.get_queryset()." % {
+    #                 'cls': self.__class__.__name__
+    #             }
+    #         )
+    #     ordering = self.get_ordering()
+    #     if ordering:
+    #         if isinstance(ordering, str):
+    #             ordering = (ordering,)
+    #         queryset = queryset.order_by(*ordering)
+    #
+    #     return queryset
 
 
 class SubjectListView(ListView):
