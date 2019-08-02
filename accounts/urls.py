52eb62825bdc48acfb1817613703390897b07062
Yangsu0 <yjy0186@naver.com>
2019-08-02 17:03:08 +0900

    edit profile done

--- accounts/urls.py
+++ accounts/urls.py
@@ -6,5 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', CreateUserView.as_view(), name='signup'),
     path('done/', CreateDoneUserView.as_view(), name='done'),
-    path('password_reset/', PasswordResetView.as_view(), name='passwordreset')
+    # path('password_reset/', PasswordResetView.as_view(), name='passwordreset'),
+    path('edit/<int:pk>/', UpdateUserView.as_view(), name='edit'),
 ]
