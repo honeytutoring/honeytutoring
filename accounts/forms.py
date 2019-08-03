298d9a6ce6a3d66772d275b88fa918c3b6230527
ChaeMoon <jungchaemoon@gmail.com>
2019-08-03 13:14:07 +0900

    Update account modify page

--- accounts/forms.py
+++ accounts/forms.py
@@ -27,13 +27,18 @@ class CreateUserForm(UserCreationForm):
         return user
 
 
-class AccountUpdateForm(forms.ModelForm):
-
-    class Meta:
-        model = Users
-        fields = (
-            'username',
-            'first_name',
-            'last_name',
-            ''
-        )
+# class AccountUpdateForm(forms.ModelForm):
+#
+#     class Meta:
+#         model = Users
+#         fields = (
+#             'username',
+#             'first_name',
+#             'last_name',
+#             'email',
+#             'password1',
+#             'password2',
+#             'sex',
+#             'user_type',
+#             'university',
+#         )
--- accounts/urls.py
+++ accounts/urls.py
@@ -6,7 +6,6 @@ app_name = 'accounts'
 urlpatterns = [
     path('signup/', CreateUserView.as_view(), name='signup'),
     path('done/', CreateDoneUserView.as_view(), name='done'),
-    # path('password_reset/', PasswordResetView.as_view(), name='passwordreset'),
-    path('edit/<int:pk>/', UpdateUserView.as_view(), name='edit'),
-    path('update/', AccountUpdateView.as_view(), name='account_update'),
+    path('edit/<int:pk>', UpdateUserView.as_view(), name='edit'),
+    #path('update/', AccountUpdateView.as_view(), name='account_update'),
 ]
