648f9ea609aca131564badb859c909095e9b2ee7
ChaeMoon <jungchaemoon@gmail.com>
2019-08-07 01:38:56 +0900

    Update User model and signup

--- accounts/admin.py
+++ accounts/admin.py
@@ -1,5 +1,7 @@
 from django.contrib import admin
-from .models import Users
-# Register your models here.
+from .models import *
+
 
 admin.site.register(Users)
+admin.site.register(Teacher)
+admin.site.register(Student)
