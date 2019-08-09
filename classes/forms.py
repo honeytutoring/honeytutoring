4e3385ba35b1f562f1ea6af18cc7ab6992bba536
ChaeMoon <jungchaemoon@gmail.com>
2019-08-09 18:14:13 +0900

    Updated class model and page

--- classes/forms.py
+++ classes/forms.py
@@ -1,19 +1,16 @@
 from django import forms
-from .models import Class, Schedule
+from .models import Class, Schedule, Memo
 
 
-class   ClassCreateForm(forms.ModelForm):
-    student_id = forms.CharField(max_length=150)
-    parent_id = forms.CharField(max_length=150)
-
+class ClassCreateForm(forms.ModelForm):
     class Meta:
         model = Class
-        exclude = (
-            'class_teacher',
-            'students',
-            'memo',
+        fields = (
+            'class_name',
+            'description',
+            'subject',
+            'etc',
         )
-        fields = '__all__'
 
     def __init__(self, class_teacher, *args, **kwargs):
         self.class_teacher = class_teacher
@@ -37,3 +34,18 @@ class ScheduleCreateForm(forms.ModelForm):
     def save(self, commit=True):
         self.instance.klass_id = self.klass_id
         return super(ScheduleCreateForm, self).save(commit=commit)
+
+
+class MemoCreateForm(forms.ModelForm):
+    class Meta:
+        model = Memo
+        exclude = ('klass',)
+        fields="__all__"
+
+    def __init__(self, klass_id, *args, **kwargs):
+        self.klass_id = klass_id
+        super(MemoCreateForm, self).__init__(*args, **kwargs)
+
+    def save(self, commit=True):
+        self.instance.klass_id = self.klass_id
+        return super(MemoCreateForm, self).save(commit=commit)
